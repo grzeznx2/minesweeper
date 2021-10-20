716718eddb03865df6d685232a7622c33605e43f
grzeznx2 <grzeznx2@gmail.com>
2021-10-20 23:03:45 +0200

    add Face

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,4 +1,5 @@
 import Body from '../Body'
+import Face from '../Face'
 import Header from '../Header'
 import NumberDisplay from '../NumberDisplay'
 import './App.scss'
@@ -8,6 +9,7 @@ const App: React.FC = () => {
     <div className="app">
       <Header>
         <NumberDisplay value={0} />
+        <Face />
         <NumberDisplay value={40} />
       </Header>
       <Body />
---
+++ src/components/Face/index.tsx
@@ -0,0 +1,13 @@
+import './Face.scss'
+
+const Face = () => {
+  return (
+    <div className="face">
+      <span role="img" aria-label="face">
+        😄
+      </span>
+    </div>
+  )
+}
+
+export default Face
